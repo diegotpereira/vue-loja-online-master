82ca743cc13c8a3b63cf51fb3a64c0fe43ea2e48
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-08-26 08:23:51 -0300

    alteração de espaçamento no template

--- src/components/PaginaProdutos/Item.vue
+++ src/components/PaginaProdutos/Item.vue
@@ -4,9 +4,9 @@
         <nav class="row justify-content-center" aria-label="breadcrumb">
             <ol class="breadcrumb">
                 <li class="breadcrumb-item">
-                    <router-link to="/">Principal</router-link>
+                    <router-link to="/">Principal / </router-link>
                 </li>
-                <li class="breadCrumb-item active" aria-content="page">Produtos</li>
+                <li class="breadCrumb-item active" aria-current="page">Produtos</li>
             </ol>
         </nav>
     </div>
@@ -16,3 +16,25 @@ export default {
     name: 'ItemPagina'
 }
 </script>
+<style scoped>
+.breadcrumb {
+    background: inherit !important;
+    color: #2c3539;
+    font-size: 20px;
+}
+
+.breadcrumb > li > a {
+    text-decoration: none !important;
+    color: #2c3539 !important;
+}
+
+.breadcrumb > li {
+    text-decoration: none !important;
+    color: #f2be00 !important;
+}
+
+.breadCrumb-item {
+    float: left;
+    padding-left: 5px;
+}
+</style>
